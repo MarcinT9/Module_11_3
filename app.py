86cbf693a7628d104749856e2a245aef0487f740
Marcin Topczewski <marcintopczewski90@gmail.com>
2022-09-26 17:45:39 +0200

    Change def tab3 and cut class db

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import datetime as dt
-import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -9,46 +7,9 @@ import plotly.graph_objects as go
 import tab1
 import tab2
 import tab3
-
+from class_db import db
 
 app = dash.Dash()
-
-class db:
-    def __init__(self):
-        self.transactions = db.transation_init()
-        self.cc = pd.read_csv(r'db\country_codes.csv',index_col=0)
-        self.customers = pd.read_csv(r'db\customers.csv',index_col=0)
-        self.prod_info = pd.read_csv(r'db\prod_cat_info.csv')
-
-    @staticmethod
-    def transation_init():
-        transactions = pd.DataFrame()
-        src = r'db\transactions'
-        for filename in os.listdir(src):
-            transactions = transactions.append(pd.read_csv(os.path.join(src,filename),index_col=0))
-
-        def convert_dates(x):
-            try:
-                return dt.datetime.strptime(x,'%d-%m-%Y')
-            except:
-                return dt.datetime.strptime(x,'%d/%m/%Y')
-
-        transactions['tran_date'] = transactions['tran_date'].apply(lambda x: convert_dates(x))
-
-        return transactions
-
-    def merge(self):
-        df = self.transactions.join(self.prod_info.drop_duplicates(subset=['prod_cat_code'])
-        .set_index('prod_cat_code')['prod_cat'],on='prod_cat_code',how='left')
-
-        df = df.join(self.prod_info.drop_duplicates(subset=['prod_sub_cat_code'])
-        .set_index('prod_sub_cat_code')['prod_subcat'],on='prod_subcat_code',how='left')
-
-        df = df.join(self.customers.join(self.cc,on='country_code')
-        .set_index('customer_Id'),on='cust_id')
-
-        self.merged = df
-
 df = db()
 df.merge()
 
@@ -141,16 +102,14 @@ def tab3_barh_store_subcat(chosen_cat):
     fig = go.Figure(data=data,layout=go.Layout(barmode='stack',margin={'t':20,}))
     return fig
 
-@app.callback(Output('pie-store-subcat','figure'),
+@app.callback(Output('barh-store2-subcat','figure'),
             [Input('store2_dropdown','value')])
 def tab3_pie_store_subcat(chosen_cat):
 
-    name_days = pd.transactions(start='2016-01-25', freq='D', periods=3)
-    name_days.dt.day_name()
+    grouped = df.merged[(df.merged['total_amt']>0)&(df.merged['Store_type']==chosen_cat)].groupby('tran_date')['total_amt'].sum()
+    labels = grouped.index.day_name()
 
-    grouped = df[df['total_amt']>0].groupby('Store_type')['total_amt'].sum()
-    
-    fig = go.Figure(data=[go.Pie(labels=name_days.index,values=grouped.values)])
+    fig = go.Figure(data=[go.Pie(labels=labels, values=grouped.values, sort=False)])
     return fig
 
 if __name__ == '__main__':
